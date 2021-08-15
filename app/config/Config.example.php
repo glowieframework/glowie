8e11b0ede362dbbe46817d80cc5acac966417e85
Gabriel Silva <gabriel.o.silva10@gmail.com>
2021-08-15 19:14:38 -0300

    New config structure

--- app/config/Config.example.php
+++ app/config/Config.example.php
@@ -4,7 +4,7 @@
         --------------------------------
         Application configuration
         --------------------------------
-        In this file you can define your application configuration for each environment.
+        In this file you can define your application configuration for the current environment.
         Be sure to rename this file to 'Config.php' before starting.
 
         ---------------------------------------------------------------------------------------------------------------
@@ -12,13 +12,10 @@
         ---------------------------------------------------------------------------------------------------------------
     */
 
-    # ------------------------
-    # Development environment
-    # ------------------------
+    return [
 
-    $config['development'] = array(
         // Enable application cache
-        'cache' => false,
+        'cache' => true,
 
         // Use Skeltch templating engine
         'skeltch' => true,
@@ -49,47 +46,7 @@
             'db' => 'glowie',
             'port' => 3306
         ]
-    );
 
-    # -----------------------
-    # Production environment
-    # -----------------------
-
-    $config['production'] = array(
-        // Enable application cache
-        'cache' => true,
-
-        // Use Skeltch templating engine
-        'skeltch' => true,
-
-        // Application default timezone
-        'timezone' => 'America/Sao_Paulo',
-
-        // Error reporting level
-        'error_reporting' => 0,
-
-        // Enable error logging
-        'error_log' => true,
-
-        // Unused session lifetime
-        'session_lifetime' => 120,
-
-        // Key used in encrypting functions
-        'app_key' => 'f08e8ba131c7abab97dba275fab5a85e',
-
-        // Token used in encrypting functions
-        'app_token' => 'd147723d9e91340d9dd28fbd5a0b6651',
-
-        // Default database connection settings
-        'database' => [
-            'host' => 'localhost',
-            'username' => 'user',
-            'password' => '123',
-            'db' => 'glowie',
-            'port' => 3306
-        ]
-    );
-
-    // Remember: you can create as many environments as you want to.
+    ];
 
 ?>
