c83128c61991f24d31d814a1bfe7df6daf84dfbd
Gabriel Silva <gabriel.o.silva10@gmail.com>
2021-08-25 23:36:24 -0300

    Firefly custom commands

---
+++ app/commands/Quote.php
@@ -0,0 +1,35 @@
+<?php
+    namespace Glowie\Commands;
+
+    use Util;
+    use Glowie\Core\CLI\Command;
+
+    class Quote extends Command{
+
+        /**
+         * The command script.
+         */
+        public function run(){
+            // Gets a random quote
+            $quote = Util::randomArray([
+                '"Computers are fast; programmers keep it slow."',
+                '"Programming can be fun, and so can cryptography; however, they should not be combined."',
+                '"When we had no computers, we had no programming problems either."',
+                '"There is no Ctrl-Z in life."',
+                '"Whitespace is never white."',
+                '"When all else fails … reboot."',
+                '"The computer was born to solve problems that did not exist before."',
+                '"There’s no place like 127.0.0.1."',
+                '"It works on my machine."',
+                '"A good programmer is someone who always looks both ways before crossing a one-way street."',
+                '"Software and cathedrals are much the same — first we build them, then we pray."',
+                '"It’s not a bug – it’s an undocumented feature."'
+            ]);
+
+            // Prints the quote in the console
+            $this->info($quote);
+        }
+
+    }
+
+?>
