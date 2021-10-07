6b983f5bb7eda82dce92658ed1029685d81587eb
Gabriel Silva <gabriel.o.silva10@gmail.com>
2021-10-06 23:30:14 -0300

    New language structure and view updates

--- app/languages/en.php
+++ app/languages/en.php
@@ -8,10 +8,10 @@
         Check Glowie docs to know how to get started.
     */
 
-    Babel::set('en', [
+    return [
         'forbidden' => 'You are not allowed to access this page',
         'not_found' => 'The page you were looking for was not found',
         'not_allowed' => 'The current method is not allowed'
-    ]);
+    ];
 
 ?>
