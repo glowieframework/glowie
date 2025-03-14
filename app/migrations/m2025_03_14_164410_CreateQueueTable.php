8805b3515e02d9cc4391393b2fdb4864857ced04
Gabriel Oliveira <gabriel.o.silva10@gmail.com>
2025-03-14 16:48:56 -0300

    Major updates for 1.8 release

---
+++ app/migrations/m2025_03_14_164410_CreateQueueTable.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Glowie\Migrations;
+
+use Config;
+use Glowie\Core\Database\Migration;
+use Glowie\Core\Database\Skeleton;
+
+/**
+ * Queue table migration for Glowie application.
+ * @category Migration
+ * @package glowieframework/glowie
+ * @author Glowie
+ * @copyright Copyright (c) Glowie
+ * @license MIT
+ * @link https://glowie.gabrielsilva.dev.br
+ * @see https://glowie.gabrielsilva.dev.br/docs/latest/extra/migrations
+ */
+class m2025_03_14_164410_CreateQueueTable extends Migration
+{
+
+    /**
+     * Migration database connection name (from your app configuration).
+     * @var string
+     */
+    protected $database = 'default';
+
+    /**
+     * Cache table name (from your app configuration).
+     * @var string
+     */
+    private $table = 'queue';
+
+    /**
+     * This method will be called before any other methods from this migration.
+     */
+    public function init()
+    {
+        $this->database = Config::get('queue.connection', 'default');
+        $this->table = Config::get('queue.table', 'queue');
+    }
+
+    /**
+     * Runs the migration.
+     * @return bool Returns true on success or false on errors.
+     */
+    public function run()
+    {
+        return $this->forge->table($this->table)
+            ->id()
+            ->createColumn('job')->type(Skeleton::TYPE_TEXT)
+            ->createColumn('queue')->type(Skeleton::TYPE_STRING)->size(255)
+            ->createColumn('data')->type(Skeleton::TYPE_LONG_BLOB)->nullable()
+            ->createColumn('added_at')->type(Skeleton::TYPE_DATETIME)->default(Skeleton::raw('NOW()'))
+            ->createColumn('delayed_to')->type(Skeleton::TYPE_DATETIME)->nullable()
+            ->createColumn('ran_at')->type(Skeleton::TYPE_DATETIME)->nullable()
+            ->createColumn('attempts')->type(Skeleton::TYPE_INTEGER)->unsigned()->default('0')
+            ->createColumn('errors')->type(Skeleton::TYPE_LONG_TEXT)->nullable()
+            ->ifNotExists()
+            ->create();
+    }
+
+    /**
+     * Rolls back the migration.
+     * @return bool Returns true on success or false on errors.
+     */
+    public function rollback()
+    {
+        return $this->forge->table($this->table)
+            ->ifExists()
+            ->drop();
+    }
+}
